4396decab0d1da42e39e2cb9331ab42b8f18cd2c
Steven <steven.lariscy@gmail.com>
2017-02-01 19:58:41 -0500

    finished DrawCube

---
+++ src/DrawCube.java
@@ -0,0 +1,23 @@
+/**
+ * @author Steven
+ */
+public class DrawCube {
+    
+    public static void main(String[] args) {
+        System.out.println("     **************");
+        System.out.println("    *            **");
+        System.out.println("   *            * *");
+        System.out.println("  *            *  *");
+        System.out.println(" *            *   *");
+        System.out.println("**************    *");
+        System.out.println("*            *    *");
+        System.out.println("*            *    *");
+        System.out.println("*            *    *");
+        System.out.println("*            *   * ");
+        System.out.println("*            *  *  ");
+        System.out.println("*            * *   ");
+        System.out.println("*            **    ");
+        System.out.println("**************     ");
+    }
+
+}
