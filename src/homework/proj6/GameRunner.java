97ab46dda7fb53ecb4daf0842d9a1a87fe5e7ccd
steven lariscy <steven.lariscy@bcbssc.com>
2017-03-08 17:18:55 -0500

    project 6 final touch

--- src/homework/proj6/GameRunner.java
+++ src/homework/proj6/GameRunner.java
@@ -24,10 +24,10 @@ public class GameRunner {
         players = new Player[2];
         spinner = new Spinner();
         r = new Random();
-    }
+    } // end of contructor
 
     private void play() {
-        this.log("Welcome to Chutes and Ladders!");
+        System.out.println("Welcome to Chutes and Ladders!");
         
         this.setupPlayers();
         this.determinePlayerOrder();
@@ -36,68 +36,66 @@ public class GameRunner {
         while (!someoneWon){
             for (Player p : players){
                 int spinAmount = spinner.spin();
-                this.log(p.getName() + ": spun a " + spinAmount);
+                System.out.println(p.getName() + ": spun a " + spinAmount);
                 
                 int currentSquare = p.getCurrentSquareNumber();
                 int currentPlusSpin = currentSquare + spinAmount;
                 
                 if (currentPlusSpin > BoardLogic.MAX_BOARD_NUMBER){
-                    this.log(p.getName() + "'s current spin would have moved "
+                    System.out.println(p.getName() + "'s current spin would have moved "
                         + "them past " + BoardLogic.MAX_BOARD_NUMBER + 
                         " so they will remain still");
                 } else {
                     p.updateCurrentSquareNumber(BoardLogic.getNextSqure(currentPlusSpin));
-                    this.log(p.getName() + ": moved to " + p.getCurrentSquareNumber());
+                    System.out.println(p.getName() + ": moved to " + p.getCurrentSquareNumber());
                     
                     if (p.getCurrentSquareNumber() == BoardLogic.MAX_BOARD_NUMBER){
-                        this.log(p.getName() + " has won the game! Congratulations!");
+                        System.out.println(p.getName() + " has won the game! Congratulations!");
                         someoneWon = true;
-                        break; // breaks out of for loop
+                        break; // break out of for loop
                     }
                 }
             }
         }
         
-        this.log("Thanks for playing!");
+        System.out.println("Thanks for playing!");
     } // end of play
     
     private void setupPlayers(){
         Scanner s = new Scanner(System.in);
         Player p1 = null;
-        while (p1 == null){
-            //@TODO make this better
-            this.logNoLine("Player 1, enter your name: ");
+        while (p1 == null || p1.getName().trim().equalsIgnoreCase("")){
+            System.out.print("Player 1, enter your name: ");
             p1 = new Player(s.nextLine());
             players[0] = p1;
         }
         Player p2 = null;
-        while (p2 == null){
-            //@TODO make this better
-            this.logNoLine("Player 2, enter your name: ");
+        while (p2 == null || p2.getName().trim().equalsIgnoreCase("")){
+            System.out.print("Player 2, enter your name: ");
             p2 = new Player(s.nextLine());
             players[1] = p2;
         }
         s.close();
         
-        this.log(
+        System.out.println(
             String.format("Current match: %s vs. %s. Good luck!"
                 , players[0].getName(), players[1].getName()));
     } // end of setupPlayers
     
     private void determinePlayerOrder() {
-        this.log("Flipping a coin to see who goes first");
+        System.out.println("Flipping a coin to see who goes first");
         
         char[] flipSegments = {'-', '\\', '|', '/'};
         
         for (int i = 0; i < (r.nextInt(15) + 1); i++){
-            this.log(flipSegments[i % flipSegments.length]);
+            System.out.println(flipSegments[i % flipSegments.length]);
             try {
                 Thread.sleep(500);
             } catch (InterruptedException ex) { /* ignore exception */ }
         }
         
         boolean isHeads = r.nextBoolean();
-        this.log("Flip landed on " + (isHeads ? "Heads" : "Tails") + 
+        System.out.println("Flip landed on " + (isHeads ? "Heads" : "Tails") + 
             " so " + (isHeads ? players[0].getName() : players[1].getName()) + 
             " will start");
         
@@ -107,18 +105,4 @@ public class GameRunner {
         }
     } // end of determinePlayerOrder
     
-    /*
-    Utility methods below
-    @TODO fix all these - this is terrible
-    */
-    private void log(char c){
-        log(Character.toString(c));
-    }
-    private void log(String line){
-        System.out.println(line);
-    }
-    private void logNoLine(String line){
-        System.out.print(line);
-    }
-
 } // end of class
