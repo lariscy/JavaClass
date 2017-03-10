8252f68d3273a1188c725263975bd7d021b6e4f7
steven lariscy <steven.lariscy@bcbssc.com>
2017-03-10 17:07:06 -0500

    working on homework.proj7

---
+++ src/homework/proj7/PrintDiamond2.java
@@ -0,0 +1,39 @@
+package homework.proj7;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.stream.Stream;
+
+/**
+ * @author Steven Lariscy
+ */
+public class PrintDiamond2 {
+    
+    private static final char[] VALID_DIAMOND_CHARS = {'*', '$'};
+    
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        
+        System.out.print("Enter number: ");
+        int inputNum = s.nextInt();
+        
+        int totalRows = inputNum * 2 - 1;
+        for (int row = 1; row <= totalRows; row++){
+            
+            // print spaces
+            if ((inputNum > 1 && row < inputNum) || (inputNum > 1 && row > inputNum)){
+                System.out.print(String.format("%" + Math.abs(inputNum - row) + "s", " "));
+            }
+            
+            // print chars
+            for (int j = 0; j < ((row > inputNum ? (totalRows - row + 1) : row) * 2 - 1); j++){
+                System.out.print(VALID_DIAMOND_CHARS[j % VALID_DIAMOND_CHARS.length]);
+            }
+            
+            System.out.println("");
+        }
+        
+        s.close();
+    } // end of main
+    
+} // end of class
